7f8fa1fff2049f48307c9a57b190e08c7e81547f
iliassid <enaailiass@gmail.com>
2024-12-25 10:56:35 +0100

    first commit

---
+++ src/User.java
@@ -0,0 +1,46 @@
+public class User extends Person{
+    String email,password;
+    Role role;
+
+    public User(String name, int age,String email,String password, Role role) {
+        super(name, age);
+        this.email=email;
+        this.password=password;
+        this.role=role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "role=" + role +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/UserManagement.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class UserManagement {
+    ArrayList<Role>roles=new ArrayList<>();
+    ArrayList<User>users=new ArrayList<>();
+    public void AddUser(){
+
+    }
+}
