7120a13692923803363f76192e91d4e4adf23f86
iliassid <enaailiass@gmail.com>
2024-12-27 10:26:48 +0100

    added function

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,35 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         UserManagement usermanagement = new UserManagement();
-        System.out.println("Enter your choice:");
-        System.out.println("1 add new user");
-        System.out.println("2 search for a user");
-        System.out.println("3 edit a user info");
-        System.out.println("4 delete a user");
-        System.out.println("5 show users");
-        System.out.println("6 quit");
-        int choice=scanner.nextInt();
-        scanner.nextLine();
-        if (choice == 6){
-            System.out.println("goodbye!");
-        }
 
 
 
-        while (choice !=6) {
 
-
-            choice = scanner.nextInt();
+        while (true) {
+            System.out.println("Enter your choice:");
+            System.out.println("1 add new user");
+            System.out.println("2 search for a user");
+            System.out.println("3 edit a user info");
+            System.out.println("4 show users");
+            System.out.println("5 delete a user");
+            System.out.println("6 quit");
+            int choice=scanner.nextInt();
             scanner.nextLine();
-
             switch (choice) {
                 case 1:
                     usermanagement.AddUser();
                     break;
                 case 2:
                     usermanagement.SearchUser();
+                    break;
+                case 3:
+                    usermanagement.editUser();
+                    break;
+                case 4:
+                    usermanagement.displayUsers();
+                    break;
+                case 5:
+                    usermanagement.deleteUser();
                     break;
                 case 6:
                     System.out.println("goodbye!");
@@ -41,5 +43,7 @@ public class Main {
 
 
         }
+
+
     }
 }
--- src/UserManagement.java
+++ src/UserManagement.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class UserManagement {
     ArrayList<User>users=new ArrayList<>();
-    ArrayList<Role>roles=new ArrayList<>();
+    //ArrayList<Role>roles=new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
     String email;
 
@@ -17,7 +17,6 @@ public class UserManagement {
         System.out.println("Enter the user age: ");
         int age = scanner.nextInt();
         scanner.nextLine();
-
         while (true){
             System.out.println("Enter the user email: ");
             email = scanner.nextLine();
@@ -27,10 +26,9 @@ public class UserManagement {
             if (!toFind){
                 System.out.println("you have to include @ in the email");
             }else{
-                System.out.println("you Entered a valide email");
+                System.out.println("you Entered a valid email");
                 break;
             }
-
         }
         System.out.println("Enter the password: ");
         String password = scanner.nextLine();
@@ -48,7 +46,8 @@ public class UserManagement {
         System.out.println("Enter name, email or role to search for the user ");
         String toSearch = scanner.nextLine();
         for (User user: users){
-            if (user.getName().contains(toSearch) || user.getEmail().contains(toSearch) || user.getRole().getRoleName().contains(toSearch)){
+            if (user.getName().contains(toSearch) || user.getEmail().contains(toSearch)
+                    || user.getRole().getRoleName().contains(toSearch)){
                 stock.add(user);
             }
         }
@@ -60,5 +59,64 @@ public class UserManagement {
             System.out.println(user);
         }
 
+    }
+    public void editUser(){
+        System.out.println("Enter the email of the account to update:");
+        String accountEmail = scanner.nextLine();
+        System.out.println("1.edit name");
+        System.out.println("2.edit age");
+        System.out.println("3.edit email");
+        System.out.println("4.edit role");
+        System.out.println("5.edit password");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        for (User user: users){
+            if (user.getEmail().equals(accountEmail)){
+                switch (choice){
+                    case 1:
+                        System.out.println("Enter the new name: ");
+                        user.setName(scanner.nextLine());
+                        break;
+                    case 2:
+                        System.out.println("Enter the new age: ");
+                        user.setAge(scanner.nextInt());
+                        break;
+                    case 3:
+                        System.out.println("Enter the new email: ");
+                        user.setEmail(scanner.nextLine());
+                        break;
+                    case 4:
+                        System.out.println("Enter the new role: ");
+                        String rolename = scanner.nextLine();
+                        Role role = new Role(rolename);
+                        user.setRole(role);
+                        break;
+                    case 5:
+                        System.out.println("Enter the new password: ");
+                        user.setPassword(scanner.nextLine());
+                        break;
+                }
+                System.out.println("User updated successfully");
+                return;
+            }
+            System.out.println("User not found");
+        }
+
+
+
+    }
+    public void displayUsers(){
+        if (users.isEmpty()){
+            System.out.println("No user has been found");
+        }
+        for (User user: users){
+            System.out.println(user);
+        }
+    }
+    public void deleteUser(){
+        System.out.println("Enter the email of the user to delete");
+        email = scanner.nextLine();
+        users.removeIf(user -> user.getEmail().equals(email));
+        System.out.println("The user removed successfully");
     }
 }
